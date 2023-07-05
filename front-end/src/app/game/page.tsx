e508c1aed3242187acbf58d92dda9476ca53151a
benJ3D <benjamin@ben3d.fr>
2023-07-06 01:42:23 +0200

    Add button dbg goal and real time score refresh

--- front-end/src/app/game/page.tsx
+++ front-end/src/app/game/page.tsx
@@ -1,16 +1,21 @@
+'use client'
+
 import React from 'react'
 import './game.css'
+import ButtonDBG from '@/Components/button/btn_dbg';
 
-let scoreP1 = 1;
-let scoreP2 = 2;
+// let scoreP1 = 1;
+// let scoreP2 = 2;
 
 interface playerScoreboard {
 	playerId: number;
 	positionInGame: 'left' | 'right';
+	score: number;
 }
 
 function Background () {
 	return (
+		
 		<>
 		
 		</>
@@ -26,47 +31,61 @@ function Player() {
 	return ret
 }
 
-function getPlayerScore(player: playerScoreboard)
-{
-	return player.positionInGame === 'left' ? scoreP1 : scoreP2
-}
+// function getPlayerScore(player: playerScoreboard)
+// {
+// 	return player.positionInGame === 'left' ? scoreP1 : scoreP2
+// }
 
-function addGoal(position: 'left' | ' right' ): void{
-	if (position === 'left')
-		scoreP1++;
-	else
-		scoreP2++;
-	console.log("Score:\nP1: " + scoreP1 + '\nP2: ' + scoreP2 );
-}
+// function addGoal(position: 'left' | 'right' ): void{
+// 	if (position === 'left')
+// 		scoreP1++;
+// 	else
+// 		scoreP2++;
+// 	console.log("Score:\nP1: " + scoreP1 + '\nP2: ' + scoreP2 );
+// }
+
+
 
 function ScoreDisplay({player}: {player: playerScoreboard})
 {
 	return (
 		player.positionInGame === 'left' ?
-		<div className="text-center text-teal-700 text-[5vw] font-normal">{getPlayerScore(player)}</div> :
+		<div className="text-center text-teal-700 text-[5vw] font-normal">{player.score}</div> :
 		<div className='flex justify-end'>
-			<div className="text-center text-teal-700 text-[5vw] font-normal">{getPlayerScore(player)}</div> 
+			<div className="text-center text-teal-700 text-[5vw] font-normal">{player.score}</div> 
 		</div>
 	)
 }
 
-export default function page() {
-
+export default function Page() {
+	const [scoreP1, setScoreP1] = React.useState(0);
+	const [scoreP2, setScoreP2] = React.useState(0);
 	
+	function addGoal(position: 'left' | 'right'): void {
+		if (position === 'left') {
+			setScoreP1(scoreP1 + 1);
+		} else {
+			setScoreP2(scoreP2 + 1);
+		}
+	}
+
 	return (
 		<main className=' bg-blue-app min-h-screen py-[13vw]'>
-
 			<section className='flex justify-center'>
+					<ButtonDBG param={{f: () => addGoal('left'), text: 'ADD GOAL P1'}}/>
 				<div className="flex justify-between w-1/3 bg-blue-game rounded-2xl" 
 					style={{ height: "calc(40vw * 10 / 16) " }}>
 					<Player />
-					<ScoreDisplay player={{playerId: 1, positionInGame:'left'}}/>
-					<ScoreDisplay player={{playerId: 2, positionInGame:'right'}}/>
+					<ScoreDisplay player={{playerId: 1, positionInGame:'left', score: scoreP1}}/>
+					<ScoreDisplay player={{playerId: 2, positionInGame:'right', score: scoreP2}}/>
 					<Player />
 			 	</div>
+
+					<ButtonDBG param={{f: () => addGoal('right'), text: 'ADD GOAL P2'}}/>
 			</section>
 		</main>
 
 	)
 }
 {/* <div className="boxgame"></div> */}
+
