e241113d3da9f75888964bcd0e8fcd419ba7484c
benJ3D <benjamin@ben3d.fr>
2023-07-12 19:28:48 +0200

    init fork + add remove request

--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -41,7 +41,11 @@ export class UsersService {
 		return `This action updates a #${id} user`;
 	}
 
-	remove(id: number) {
-		return `This action removes a #${id} user`;
+	// remove(id: number) {
+	// 	return `This action removes a #${id} user`;
+	// }
+	async remove(id: number): Promise<string> {
+		await this.usersRepository.delete(id);
+		return `User with id ${id} has been removed successfully`;
 	}
 }
