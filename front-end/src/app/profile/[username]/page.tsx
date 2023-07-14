55efe2bad5583eec54d108771c62ae9cc7abfc77
benJ3D <benjamin@ben3d.fr>
2023-07-14 13:32:00 +0200

    update get user method

--- front-end/src/app/profile/[username]/page.tsx
+++ front-end/src/app/profile/[username]/page.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function ProfileUser({params}: {params: { username: string}}) {
 
-	//reauete api pour avoir les infos de l'user sur localhost:8000/api/users/username
+	const [tmp, setTmp] = useState<string>('NON');
+	
 
+	console.log('PorfileUser HelloWorld');
+	
+	useEffect(() => {
+		
+		console.log('PorfileUser useEffect HelloWorld');
+
+		//requete api pour avoir les infos de l'user sur localhost:8000/api/users/username
+		axios.get(`/localhost:8000/api/users/username/${params.username}`)
+		.then((reponse) => {
+			setTmp('query status = ' + reponse.status);
+		})
+		.catch((e) => {
+			console.log(e);
+		})
+	});
+		
 	return (
 		<div>ProfileUser {params.username}</div>
 	)
